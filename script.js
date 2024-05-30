5a31a1c4171990bcdc1520ec4a30debbf3fa91d0
Feroz <kfkhalid2004@gmail.com>
2024-05-30 14:25:42 +0500

    basics are complete (done by tutorial)

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
-let main = document.querySelector('main');
 
-let div = "";
 
-for (let i = 0; i < 252; i++) {
-    div += `<div>${" "}</div>`;
+function populateBoard(size){
+
+let board = document.querySelector('.board');
+let squares = board.querySelectorAll('div');
+squares.forEach(div=> div.remove());
+board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+let amount = size * size;
+for (let i = 0; i < amount; i++) {
+   let square = document.createElement('div');
+   square.addEventListener("mouseover", () => {square.style.backgroundColor = "black"});
+   square.style.backgroundColor = "blue";
+   board.insertAdjacentElement("beforeend", square)
 }
 
+}
+
+populateBoard(16);
+
+function changeSize(input) {
+    if (input <= 100){
+        populateBoard(input);
+    } else {
+        console.log("too many squares");
+     }
+}
+/*
 main.innerHTML = div;
 
 var reset = document.getElementById('reset');
@@ -17,4 +39,4 @@ divElements.forEach(function(div) {
     reset.addEventListener("click", function() {
         div.style.backgroundColor = "red";
     });
-});
+});*/
